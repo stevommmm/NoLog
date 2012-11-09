0eec60f16b9bfc627f89b47eac06799984855cda
Stephen Mcgregor <tehborris@gmail.com>
2012-11-10 07:50:03 +1000

    revert to older code, projectile and distance modification

--- src/com/c45y/NoLog/NoLogListener.java
+++ src/com/c45y/NoLog/NoLogListener.java
@@ -1,10 +1,13 @@
 package com.c45y.NoLog;
 
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -13,6 +16,8 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class NoLogListener implements Listener{
 	public final NoLog plugin;
@@ -45,13 +50,13 @@ public class NoLogListener implements Listener{
 			NoLogObject nlo = new NoLogObject((Player)damageEvent.getDamager(),System.currentTimeMillis(),player.getLocation());
 			plugin.PlayerLog.put(player, nlo);
 		}
-		else if (damageEvent.getDamager() instanceof Arrow) {
-			Arrow arrow = (Arrow) damageEvent.getDamager();
-			if (arrow.getShooter() instanceof Player) {
+		else if (damageEvent.getDamager() instanceof Projectile) {
+			Projectile projectile = (Projectile) damageEvent.getDamager();
+			if (projectile.getShooter() instanceof Player) {
 				Player player = (Player) event.getEntity();
-				Player shooter = (Player) arrow.getShooter();
+				Player shooter = (Player) projectile.getShooter();
 				if (player != shooter) {
-					NoLogObject nlo = new NoLogObject(shooter,System.currentTimeMillis(),player.getLocation());
+					NoLogObject nlo = new NoLogObject(shooter, System.currentTimeMillis() ,player.getLocation());
 					plugin.PlayerLog.put(player, nlo);
 				}
 			}
@@ -61,24 +66,61 @@ public class NoLogListener implements Listener{
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
-//		if (player.isDead()) { return; }
 		if (player.getFallDistance() > 4F ) {
 			System.out.println("*NL: " + player.getName() + " tried to avoid '" + player.getFallDistance() + "' fall damage.");
 		}
 		if (plugin.PlayerLog.containsKey(player)) {
-			// Check if the attacker is dead, we don't care if you log after killing someone
-			NoLogObject nlo = plugin.PlayerLog.get(player);
-			if ((!nlo.getAttacker().isDead()) && (nlo.getTimestamp() > System.currentTimeMillis() - 10000) && (nlo.getDistance(nlo.getAttacker()) < 50)) {
-				plugin.messageMods(ChatColor.BLUE + "NL: " + genNoLogMessage(player));
-				plugin.log.info("NoLog: " + genNoLogMessage(player));
+			if (!containsPlayer(player.getNearbyEntities(4, 4, 4))) { 
+				return; // Check we actually have players close by
 			}
+			if (isInventoryEmpty(player.getInventory())) { 
+				return; // Do we really care if they have nothing anyway?
+			}
+			NoLogObject nlo = plugin.PlayerLog.get(player);
 			plugin.PlayerLog.remove(player);
+			if (player.getTicksLived() < 200) {
+				return; // Player is not a suitable age
+			}
+			if (nlo.getAttacker().getTicksLived() < 200) {
+				return; // Attacker is not a suitable age
+			}
+			if (nlo.getTimestamp() > System.currentTimeMillis() - 10000) {
+				return; // PVP was more than 10 seconds ago
+			}
+			if (nlo.getDistance(nlo.getAttacker()) < 50) {
+				return; // They are a large distance from the attacker
+			}
+			plugin.messageMods(ChatColor.BLUE + "NL: " + genNoLogMessage(player));
+			plugin.log.info("NoLog: " + genNoLogMessage(player));
+			if (plugin.chicken) {
+				event.setQuitMessage(event.getPlayer() + " chickened out");
+				Location loc = player.getLocation();
+	        	loc.getWorld().spawnEntity(loc, EntityType.CHICKEN);
+			}
 		}
-		if (plugin.chicken) {
-			Location loc = player.getLocation();
-        	loc.getWorld().spawnEntity(loc, EntityType.CHICKEN);
+	}
+	
+	private boolean containsPlayer(List<Entity> entityList) {
+		for( Entity e : entityList) {
+			if(e.getClass().isInstance(Player.class)) {
+				return true;
+			}
 		}
-
+		return false;
+	}
+	
+	private boolean isInventoryEmpty(PlayerInventory pli) {
+		for (final ItemStack is : pli.getArmorContents()) {
+			if (is != null) {
+				return false;
+			}
+		}
+		for (final ItemStack is : pli.getContents()) {
+			if (is != null) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	// Generate the resulting string here to keep it all neat and easy to change.
